6a49471cde20a9474abc800ecbd29fa50827c142
vishal <vishalsharma.vs546@gmail.com>
2020-11-03 11:38:41 +0530

    Create project in Studio.Create LogDebug for easy debugging

---
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -0,0 +1,13 @@
+package com.example.logutil;
+
+public class LogDebug {
+
+    public static final String TAG="SUPER_AWESOME_APP";
+
+    public static void d(String message)
+    {
+        
+    }
+
+
+}
